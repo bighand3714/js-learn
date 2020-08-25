5c8dbb62057cbb1ad4ac98e95673d461b6ece71a
bighand3714 <bighand3714@gmail.com>
2020-08-25 22:43:35 +0800

    0.6

--- 算法常见手写题/09-动态规划.js
+++ 算法常见手写题/09-动态规划.js
@@ -8,15 +8,15 @@
 // 通用解题思路：申请一个 dp 数组，明确数组中每个元素代表的含义，找到状态转移方程和 base case。一般用于求最值。
 
 // 斐波拉契数
-function Fibonacci(n){
-    if(n<=1){
+function Fibonacci(n) {
+    if (n <= 1) {
         return n;
     }
     let i = 1;
     let pre = 0;
     let current = 1;
     let result = 0;
-    while(i++ < n){
+    while (i++ < n) {
         result = pre + current;
         pre = current;
         current = result;
