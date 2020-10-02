d93eb3526af54a53dcadcd35fd33194ef97acf5c
bighand3714 <bighand3714@gmail.com>
2020-10-02 22:29:23 +0800

    0.1

--- webpack/webpack.js
+++ webpack/webpack.js
@@ -1,25 +1,23 @@
-(function(modules) {
-  //...
-})({
-  './src/index.js': function (module, exports) {
-    eval(
-      'console.log("hello webpack"); \n\n\n//# sourceURL=webpack'
-    )
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+
+module.exports = {
+  entry: __dirname + '/app/main.js',
+  output: {
+    path: __dirname + '/build',
+    filename: 'bundle-[hash].js',
+  },
+  devtool: 'none',
+  devServer: {
+    contentBase: './public',
+    historyApiFallback: true,
+    inline: true,
+    hot: true,
+  },
+  module: {
+    rules: [{
+      
+    }]
   }
-})
-
-var installedModules = {}
-
-function __webpack_require__(moduleId) {
-  __webpack_require__.p = ""
-  return __webpack_require__((__webpack_require__.s = './src/index.js'))
-}
-
-module: {
-  rules: [
-    {
-      test: /\.css$/,
-      use: ["style-loader"],
-    }
-  ]
 }
